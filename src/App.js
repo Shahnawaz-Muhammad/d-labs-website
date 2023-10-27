90ebaf93f893023233de94bc6075e6d3e3dab020
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-10-27 14:12:59 +0500

    header file, fonts and color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1 className='text-4xl font-bold '>hello world</h1>
+     <h1 className='text-4xl font-bold bg-green-200'>hello world</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "#fff",
+        primary: "#262626",
+        secondary: "#E45F4E",
+        lightGray: "#848484",
+        bgLight: "#f4f4f4"
+        
+
+      },
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif']
+      },
+    },
   },
   plugins: [],
-}
+};
