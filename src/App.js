b3aa8ee09e4d6db3420412792b57fbeebd33309c
FahadKhan111 <ak0902586@gmail.com>
2023-10-30 17:44:00 +0500

    about,  amenities, mission component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/home";
-import Contact from "./pages/contact";
-import Features from "./pages/features";
-import Pricing from "./pages/pricing";
-import About from "./pages/about";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import { AnimatePresence } from "framer-motion";
+import './App.css';
+import Header from './components/header';
+import LearnMore from './components/learn-more';
+import AboutUs from './components/AboutUs'
+import Features from './components/features';
+import Hero from './components/hero';
+import OurBenifits from './components/OurBenifits';
+import Mission from './components/Mission'
 
 function App() {
   return (
     <div className="App">
-
-      <AnimatePresence mode="wait">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About title="About" />} />
-          <Route path="/features" element={<Features title="Features" />} />
-          <Route path="/pricing" element={<Pricing title="Pricing" />} />
-          <Route path="/contact" element={<Contact title="Contact" />} />
-        </Routes>
-        <Footer />
-      </AnimatePresence>
+     <Header />
+     <Hero />
+     <Features />
+     <LearnMore />
+     <Header />
+     <LearnMore />
+     <AboutUs />
+     <OurBenifits />
+     <Mission />
     </div>
   );
 }
